a4744ca5b2aa16e403e87e7a9c0732aed07baca4
kadonomaro <denis.afer@gmail.com>
2020-02-03 21:54:06 +0700

    prod: fix git production path

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
   outputDir: 'docs',
   assetsDir: 'assets',
   runtimeCompiler: true,
-  parallel: false
+	parallel: false,
+	publicPath: process.env.NODE_ENV === 'production' ? '/form-validation/' : '/'
 }
